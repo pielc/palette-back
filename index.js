59ebca7e9ae27c927f84d40048541fd4642ca6c7
pierre <pierre@MacBook-Pro-de-Pierre.local>
2026-01-04 23:42:28 +0100

    working

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import "dotenv/config";
 import sharp from "sharp";
 import { kmeans } from "ml-kmeans";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.PUBLIC_SUPABASE_URL,
+  process.env.PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
+);
 
 async function extractPalette(imagePath) {
   const image = sharp(imagePath);
@@ -12,28 +19,56 @@ async function extractPalette(imagePath) {
     pixels.push([data[i], data[i + 1], data[i + 2]]);
   }
 
-  const res = kmeans(pixels, 7, {
-    initialization: "kmeans++",
-    maxIterations: 100,
+  const res = kmeans(pixels, 10, {
+    // initialization: "kmeans++",
+    maxIterations: 1000,
   });
 
   const palette = res.centroids.map((centroid) =>
     centroid.map((e) => Math.round(e)),
   );
 
-  console.log(rgbArrayToHex(palette));
-
   console.log(res.clusters.length);
+
+  const array = new Uint8Array(res.clusters);
+
+  const blob = new Blob([array], { type: "application/octet-stream" });
+
+  const { data: filedata, error: fileerror } = await supabase.storage
+    .from("labels")
+    .upload("test.bin", blob, {
+      contentType: "application/octet-stream",
+      upsert: true,
+    });
+
+  console.log(fileerror);
+  console.log(filedata);
+
+  const dbPalette = {};
+
+  palette.forEach((e, index) => (dbPalette[index] = rgbToHex(e)));
+
+  const { error: dberror } = await supabase
+    .from("ImageColors")
+    .insert({ image_id: "test", palette: dbPalette });
+
+  console.log(dberror);
 }
 
-function rgbArrayToHex(rgbArray) {
-  return rgbArray.map((rgb) => {
-    const [r, g, b] = rgb;
-    return (
-      "#" +
-      ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()
-    );
-  });
+function rgbToHex(rgb) {
+  const [r, g, b] = rgb;
+  return (
+    "#" +
+    ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()
+  );
 }
 
-extractPalette("./images/test.png");
+export async function loadPalette() {
+  const { data: paletteData } = await supabase.from("ImageColors").select();
+  console.log(paletteData);
+  return paletteData.pop().palette;
+}
+
+await extractPalette("./images/test.png");
+
+// await loadPalette();
